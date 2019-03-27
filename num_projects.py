b1c4365662deb7ca400d2e1d0e510f40cebed324
Sara Gholami <srghlm6@gmail.com>
2019-03-27 15:43:30 -0600

    refactoring

--- num_projects.py
+++ num_projects.py
@@ -1,27 +1,5 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-#
-# project_num = np.zeros(101)
-#
-#
-# def read_csv():
-#     with open('num_projects2.csv', 'r') as csv_file:
-#         plots = csv.reader(csv_file, delimiter=',')
-#         line_count = 0
-#         for row in plots:
-#             if line_count != 0:
-#                 project_num[int(row[0])] = float(row[1])
-#                 line_count += 1
-#             else:
-#                 line_count += 1
-#
-#
-
-#
-#
-# if __name__ == '__main__':
-#     read_csv()
-#     line_plot()
 
 project_num = []
 team_size = []
@@ -35,7 +13,6 @@ def load_pkl():
 
 
 def line_plot():
-    # index = np.arange(101)
     plt.plot(team_size, project_num, color='green', linewidth=1)
     plt.xlabel("Team size")
     plt.ylabel("Number of projects")
@@ -45,9 +22,6 @@ def line_plot():
 
 
 if __name__ == '__main__':
-    # Open database connection
 
     load_pkl()
     line_plot()
-
-    # disconnect from server
